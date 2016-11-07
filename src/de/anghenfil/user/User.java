40a3a7fef24b928927fbdced4b4ef89326053293
anghenfil <ares@anghenfil.de>
2016-11-07 13:10:15 +0100

    Added Getters/Setters

--- src/de/anghenfil/user/User.java
+++ src/de/anghenfil/user/User.java
@@ -12,5 +12,42 @@ public class User implements Serializable{
 	int health; //Actual Health
 	int ap; //Action Points -> = mana
 	int act_room; //Room where the player is
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getKlasse() {
+		return klasse;
+	}
+	public void setKlasse(String klasse) {
+		this.klasse = klasse;
+	}
+	public String getRasse() {
+		return rasse;
+	}
+	public void setRasse(String rasse) {
+		this.rasse = rasse;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getAp() {
+		return ap;
+	}
+	public void setAp(int ap) {
+		this.ap = ap;
+	}
+	public int getAct_room() {
+		return act_room;
+	}
+	public void setAct_room(int act_room) {
+		this.act_room = act_room;
 	}
 
+	
+}
